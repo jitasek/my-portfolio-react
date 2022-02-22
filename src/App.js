1829505f91e9f3ea4cb02f786c5d2c91a7d0f551
jitasek <jitka.moreira@gmail.com>
2022-02-22 13:07:19 +0100

    add Navbar, prep About Me and Resume

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import AboutMe from "./components/AboutMe";
+import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
 
 function App() {
   return (
     <div className="flex flex-col">
+      <Navbar />
       <Portfolio />
+      <AboutMe />
+      <Resume />
     </div>
   );
 }
