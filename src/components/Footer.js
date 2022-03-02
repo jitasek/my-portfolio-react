a8f85b426b5a29b650e987de4dae89dec0be4e8e
jitasek <jitka.moreira@gmail.com>
2022-03-02 17:03:04 +0100

    clean code from redundant parts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -35,10 +35,7 @@ function Footer() {
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
             >
               <svg
-                //className="w-5 h-5 fa fa-linkedin"
                 className="svg-inline--fa fa-linkedin-in w-4"
-                //fill="currentColor"
-                // viewBox="0 0 24 24"
                 viewBox="0 0 448 512"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
@@ -48,7 +45,6 @@ function Footer() {
                 role="img"
               >
                 <path
-                  //   fill-rule="evenodd"
                   fill="currentColor"
                   d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
                   clip-rule="evenodd"
