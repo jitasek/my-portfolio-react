8b60892aadbb9427a9da68c7400caa15304bf4cc
jitasek <jitka.moreira@gmail.com>
2022-04-19 16:51:48 +0200

    Add content placeholders and styles for Work section

---
+++ src/components/Work.jsx
@@ -0,0 +1,185 @@
+import React from "react";
+import WeatherImg from "../assets/weather-dash.png";
+import POTImg from "../assets/POT-login.png";
+
+const Work = () => {
+  return (
+    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#291b24]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-[#fece6c]">
+            Work
+          </p>
+          <p className="py-6">Check out some of my recent work</p>
+        </div>
+
+        {/* Container for all my projects */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* 1 Project container - grid item */}
+          <div
+            style={{ backgroundImage: `url(${POTImg})` }}
+            className="shaddow-lg shaddow-[#040c16] group container rounded-md flex justify-center items-center mx-auto mystyle-div"
+          >
+            <div>
+              {/* Hover effects */}
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  React JS Application
+                </span>
+                <div className="pt-8 text-center">
+                  <a href="/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a href="/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* 1 Project container - grid item */}
+          <div
+            style={{ backgroundImage: `url(${WeatherImg})` }}
+            className="shaddow-lg shaddow-[#040c16] group container rounded-md flex justify-center items-center mx-auto mystyle-div"
+          >
+            <div>
+              {/* Hover effects */}
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  React JS Application
+                </span>
+                <div className="pt-8 text-center">
+                  <a href="/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a href="/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* 1 Project container - grid item */}
+          <div
+            style={{ backgroundImage: `url(${WeatherImg})` }}
+            className="shaddow-lg shaddow-[#040c16] group container rounded-md flex justify-center items-center mx-auto mystyle-div"
+          >
+            <div>
+              {/* Hover effects */}
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  React JS Application
+                </span>
+                <div className="pt-8 text-center">
+                  <a href="/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a href="/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* 1 Project container - grid item */}
+          <div
+            style={{ backgroundImage: `url(${WeatherImg})` }}
+            className="shaddow-lg shaddow-[#040c16] group container rounded-md flex justify-center items-center mx-auto mystyle-div"
+          >
+            <div>
+              {/* Hover effects */}
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  React JS Application
+                </span>
+                <div className="pt-8 text-center">
+                  <a href="/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a href="/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* 1 Project container - grid item */}
+          <div
+            style={{ backgroundImage: `url(${WeatherImg})` }}
+            className="shaddow-lg shaddow-[#040c16] group container rounded-md flex justify-center items-center mx-auto mystyle-div"
+          >
+            <div>
+              {/* Hover effects */}
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  React JS Application
+                </span>
+                <div className="pt-8 text-center">
+                  <a href="/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a href="/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* 1 Project container - grid item */}
+          <div
+            style={{ backgroundImage: `url(${WeatherImg})` }}
+            className="shaddow-lg shaddow-[#040c16] group container rounded-md flex justify-center items-center mx-auto mystyle-div"
+          >
+            <div>
+              {/* Hover effects */}
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  React JS Application
+                </span>
+                <div className="pt-8 text-center">
+                  <a href="/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a href="/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
+
+// container class on line 18 --> so I can define my own width
+// mystyle-div = my own styling I define in index.css
+// because there's already the style={...} on line 19, I had to add "!important" to my hover class in index.css so that React does not ignore it
