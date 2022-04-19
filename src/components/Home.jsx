7f6b10137cd95744488aa25e1e0b364b23f7f73e
jitasek <jitka.moreira@gmail.com>
2022-04-19 13:36:05 +0200

    Add Home component content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,34 @@ import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
-  return <div className="w-full h-screen bg-[#291b24]">Home</div>;
+  return (
+    <div name="home" className="w-full h-screen bg-[#291b24]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-[#fece6c]">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Jitka Moreira
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Full Stack Developer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I am Jitka and I love to improve people's lives through technology. I
+          am a fan of a seamless user experience.
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#fece6c] hover:border-[#fece6c]">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
+
+// name="home" (line 6) added for the purpose of React smooth scroll
