5377ae513f298cc9b74ba8a94e4c02c734e79f36
jitasek <jitka.moreira@gmail.com>
2022-04-19 13:49:48 +0200

    Add About component

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-[#291b24] text-gray-300">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        {/* Container div:*/}
+        <div>
+          <div>
+            <p>About</p>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
