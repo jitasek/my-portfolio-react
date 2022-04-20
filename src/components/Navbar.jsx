e3dcadea717b58002db44202c3b4b0f95ebda3d4
jitasek <jitka.moreira@gmail.com>
2022-04-20 14:14:20 +0200

    Add react-scroll to navbar (mobile)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,6 @@
 import React, { useState } from "react";
 import Logo from "../assets/jm-logo.png";
-import {
-  FaBars,
-  FaTimes,
-  FaGithub,
-  FaLinkedin,
-  FaFacebook,
-} from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { Link } from "react-scroll";
@@ -66,11 +60,31 @@ const Navbar = () => {
             : "absolute top-0 left-0 w-full h-screen bg-[#291b24] flex flex-col justify-center items-center"
         }
       >
-        <li className="py-6 text-4xl">About</li>
-        <li className="py-6 text-4xl">Home</li>
-        <li className="py-6 text-4xl">Skills</li>
-        <li className="py-6 text-4xl">Work</li>
-        <li className="py-6 text-4xl">Contact</li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/* Social icons */}
