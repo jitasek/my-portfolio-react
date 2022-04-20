874032de5d27ad1adb1f05552c72522a0d5dd757
jitasek <jitka.moreira@gmail.com>
2022-04-20 12:16:28 +0200

    Add contact email functionality via getform.io

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,11 @@ const Contact = () => {
       name="contact"
       className="w-full h-screen bg-[#291b24] flex justify-center items-center p-4"
     >
-      <form action="" className="flex flex-col max-w-[600px] w-full">
+      <form
+        method="POST"
+        action="https://getform.io/f/839d3725-ea75-401a-aa4d-fb23b036eac0"
+        className="flex flex-col max-w-[600px] w-full"
+      >
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-[#fece6c] text-gray-300">
             Contact
